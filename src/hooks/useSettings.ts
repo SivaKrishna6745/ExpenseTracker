37ae36d051eed615ed7fc2c6bb4af618a904d44b
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-07-23 11:05:02 +0530

    feat: Update useSettings.ts

--- src/hooks/useSettings.ts
+++ src/hooks/useSettings.ts
@@ -1,11 +1,16 @@
+import { useDispatch } from 'react-redux';
 import useAppSelector from './useAppSelector';
+import { toggleTheme, setCurrency } from '../features/settings/settingsSlice';
 
 export const useSettings = () => {
+    const dispatch = useDispatch();
     const { theme, currency, autoExport } = useAppSelector((state) => state.settings.settings);
     return {
         theme,
         currency,
         autoExport,
+        setTheme: (value: 'light' | 'dark') => dispatch(toggleTheme(value)),
+        setCurrency: (value: 'INR' | 'USD' | 'EUR') => dispatch(setCurrency(value)),
     };
 };
 
