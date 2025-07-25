baaf184b437a815522ccae6feedaff7d1fed0e5d
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-07-25 12:54:18 +0530

    feat: Update SummaryCard component

--- src/components/SummaryCard.tsx
+++ src/components/SummaryCard.tsx
@@ -10,13 +10,16 @@ type SummaryCardProps = {
 
 const SummaryCard = ({ title, amount, currency, icon, color }: SummaryCardProps) => {
     return (
-        <div className="flex gap-2" style={{ backgroundColor: color || 'transparent' }}>
-            <div>
+        <div
+            className="p-4 rounded-lg flex justify-evenly items-center gap-2"
+            style={{ backgroundColor: color || 'transparent' }}
+        >
+            <div className="flex gap-2">
                 <span>{icon}</span>
                 <strong>{title}</strong>
             </div>
-            <div>
-                <span>{currency ?? '₹'}</span>
+            <div className="text-xl font-semibold">
+                <span>{currency ?? '₹'} </span>
                 <span aria-label="amount">{amount.toLocaleString()}</span>
             </div>
         </div>
