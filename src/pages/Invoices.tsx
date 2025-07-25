3d4983c2027332cf27db6c8ef40c82a1277f0fc0
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-07-25 12:56:52 +0530

    feat: Update Invoice.tsx component

--- src/pages/Invoices.tsx
+++ src/pages/Invoices.tsx
@@ -1,23 +1,91 @@
+import { ClipboardList, FilePlus2 } from 'lucide-react';
 import InvoiceCard from '../components/InvoiceCard';
 import useAppSelector from '../hooks/useAppSelector';
 import type { Invoice } from '../types/invoice';
+import { useState } from 'react';
+import InvoiceModal from '../components/InvoiceModal';
+
+const statusColorMap: { [key: string]: string } = {
+    paid: 'bg-green-400',
+    pending: 'bg-red-400',
+    draft: 'bg-gray-400',
+};
 
 const Invoices = () => {
     const invoices = useAppSelector((state) => state.invoices.invoices);
     const selectedMonth = useAppSelector((state) => state.expenses.selectedMonth);
+    const curreny = useAppSelector((state) => state.settings.settings.currency);
+
     const filteredInvoices = invoices.filter((inv: Invoice) => inv.date.startsWith(selectedMonth));
+    const invoiceCategory: {
+        [key: string]: {
+            invoices: Invoice[];
+            amount: number;
+        };
+    } = {};
+    filteredInvoices.forEach((inv: Invoice) => {
+        const invAmount = inv.items.reduce((acc, item) => acc + item.quantity * item.rate, 0);
+        if (!invoiceCategory[inv.status]) {
+            invoiceCategory[inv.status] = { invoices: [], amount: 0 };
+        }
+        invoiceCategory[inv.status].invoices.push(inv);
+        invoiceCategory[inv.status].amount += invAmount;
+    });
+
+    const [invoicesList, setInvoicesList] = useState<Invoice[]>([]);
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
 
     return (
-        <div className="flex flex-col rounded-lg bg-green-400 m-4">
+        <div className="flex flex-col m-4">
+            <div className="flex items-center justify-between cursor-pointer">
+                <h2 className="text-2xl">Invoices</h2>
+                <button
+                    className="bg-green-600 text-slate-200 p-2 rounded-lg cursor-pointer text-md font-semibold flex gap-2 justify-center items-center"
+                    onClick={() => setModalOpen(true)}
+                >
+                    <FilePlus2 />
+                    Add Invoice
+                </button>
+                <InvoiceModal
+                    open={modalOpen}
+                    onClose={() => setModalOpen(false)}
+                    invoice={undefined}
+                    onSubmit={(data) => setModalOpen(false)}
+                />
+            </div>
             {filteredInvoices?.length ? (
-                filteredInvoices.map((inv) => (
-                    <InvoiceCard
-                        key={inv.id}
-                        id={inv.id}
-                        amount={inv.items.reduce((acc, item) => acc + item.quantity * item.rate, 0)}
-                        status={inv.status}
-                        date={inv.date}
-                    />
+                Object.entries(invoiceCategory).map(([status, invoices], idx) => (
+                    <div key={idx} className="my-4 flex flex-col gap-2">
+                        <div className="px-2 py-1 rounded-full bg-red-200 text-red-700 text-xs w-max">
+                            {status
+                                .toLowerCase()
+                                .split(' ')
+                                .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
+                                .join(' ')}{' '}
+                            Invoices
+                        </div>
+                        <div className={`rounded-lg ${statusColorMap[status]}`}>
+                            <>
+                                {invoices.invoices.map((inv: Invoice) => (
+                                    <InvoiceCard
+                                        key={inv.id}
+                                        id={inv.id}
+                                        amount={inv.items.reduce((sum, item) => sum + item.rate * item.quantity, 0)}
+                                        date={inv.date}
+                                        icon={<ClipboardList size={20} className="text-gray-800" />}
+                                    />
+                                ))}
+                                <div className="flex justify-end mr-4 text-sm font-semibold tracking-wide uppercase px-3 py-2">
+                                    {status
+                                        .toLowerCase()
+                                        .split(' ')
+                                        .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
+                                        .join(' ')}{' '}
+                                    Total: {curreny ?? '₹'} {invoices.amount}
+                                </div>
+                            </>
+                        </div>
+                    </div>
                 ))
             ) : (
                 <p className="text-center text-gray-600 py-4">
