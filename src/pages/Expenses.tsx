63a2484357b08e8a8338d1df4fc06872883303e1
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-07-27 14:55:29 +0530

    feat: Add Expenses page for new route

--- src/pages/Expenses.tsx
+++ src/pages/Expenses.tsx
@@ -1,7 +1,70 @@
+import { useState } from 'react';
+import ExpenseCard from '../components/ExpenseCard';
+import MonthSelector from '../components/MonthSelector';
+import useAppSelector from '../hooks/useAppSelector';
+import type { Expense } from '../types/expense';
+import { Plus } from 'lucide-react';
+import ExpenseModal from '../components/ExpenseModal';
+
+const expEmoji: { [key: string]: string } = {
+    Travel: '🚌',
+    Food: '🍔',
+    Software: '💻',
+};
+
 const Expenses = () => {
+    const expenses = useAppSelector((state) => state.expenses.expenses);
+    // const selectedMonth = useAppSelector((state) => state.expenses.selectedMonth);
+    console.log(expenses);
+    const [selectedMonth, setSelectedMonth] = useState<string>('');
+    const handleSelectedMonth = (month: string) => {
+        setSelectedMonth(month);
+    };
+
+    const [isExpenseModalOpen, setIsExpenseModalOpen] = useState<boolean>(false);
+    const handleAddExpense = () => {
+        setIsExpenseModalOpen(true);
+    };
+
     return (
-        <div>
-            <h1>Expenses</h1>
+        <div className="mt-4 flex flex-col">
+            <div className="mb-4">
+                <MonthSelector selectedMonth={selectedMonth} onChange={handleSelectedMonth} />
+            </div>
+            {expenses.length === 0 ? (
+                <p className="text-xl text-center text-black dark:text-white">No Expenses Found 🫥</p>
+            ) : (
+                expenses.map((exp: Expense) => {
+                    return (
+                        <ExpenseCard
+                            key={exp.id}
+                            title={exp.title}
+                            amount={exp.amount}
+                            date={exp.date}
+                            category={exp.category}
+                            emoji={expEmoji[exp.category]}
+                        />
+                    );
+                })
+            )}
+            <div className="fixed bottom-6 right-6 z-50 flex flex-col items-center gap-2" onClick={handleAddExpense}>
+                <button
+                    type="button"
+                    id="add-expense"
+                    className="bg-indigo-600 text-white dark:text-slate-700 rounded-full cursor-pointer p-3 shadow-lg hover:bg-indigo-800 dark:hover:bg-indigo-400 transition-all duration-100"
+                >
+                    <Plus size={40} />
+                </button>
+                <label htmlFor="add-expense" className="text-xl cursor-pointer">
+                    Add Expense
+                </label>
+            </div>
+            <ExpenseModal
+                isOpen={isExpenseModalOpen}
+                onClose={() => {
+                    setIsExpenseModalOpen(false);
+                }}
+            />
         </div>
     );
 };
