928b61808a09914c4619edd54bd8081e42c1b4a4
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-07-29 15:51:50 +0530

    feat: Add light/dark mode toggle button in navbar chore: Remove light/dark mode toggle select element in settings page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,14 @@
+import { Moon, Sun } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import useSettings from '../hooks/useSettings';
 
 const Navbar = () => {
+    const { theme, setTheme } = useSettings();
+    const handleThemeToggle = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    };
+    const isDark = theme === 'dark';
+
     return (
         <nav className="flex items-center justify-between px-6 py-4 bg-gray-300 dark:bg-gray-800 text-gray-800 dark:text-gray-200 shadow-md">
             <div className="text-2xl font-semibold">FinanceDash</div>
@@ -18,6 +26,21 @@ const Navbar = () => {
                     Settings
                 </Link>
             </div>
+            <button
+                className={`relative inline-flex items-center h-7 w-14 rounded-full transition-colors duration-300 ${
+                    isDark ? 'bg-gray-500' : 'bg-neutral-600'
+                }`}
+                aria-label="toggle theme"
+                onClick={handleThemeToggle}
+            >
+                <span
+                    className={`inline-block h-6 w-5 transform transition-transform duration-300 ${
+                        isDark ? 'translate-x-7 text-gray-900' : 'translate-x-1 text-gray-200'
+                    }`}
+                >
+                    {theme === 'light' ? <Moon size={24} /> : <Sun size={24} />}
+                </span>
+            </button>
         </nav>
     );
 };
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -5,20 +5,6 @@ const Settings = () => {
     return (
         <div className="p-6 flex flex-col gap-6 max-w-md items-center justify-center">
             <div className="flex items-center gap-4">
-                <label htmlFor="theme-select" className="text-xl text-gray-800 dark:text-white">
-                    Theme:{' '}
-                </label>
-                <select
-                    id="theme-select"
-                    value={settings.theme}
-                    onChange={(e) => settings.setTheme(e.target.value as 'light' | 'dark')}
-                    className="bg-gray-300 text-gray-900 dark:bg-gray-800 dark:text-gray-200 px-4 py-2 rounded-md"
-                >
-                    <option value="light">Light</option>
-                    <option value="dark">Dark</option>
-                </select>
-            </div>
-            <div className="flex items-center gap-4">
                 <label htmlFor="currency-select" className="text-xl text-gray-800 dark:text-white">
                     Currency:{' '}
                 </label>
